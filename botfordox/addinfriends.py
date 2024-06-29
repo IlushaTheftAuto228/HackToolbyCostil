b36ad40b1d2fc0e15d96709139d64a0be029f709
IlushaTheftAuto228 <121304908+IlushaTheftAuto228@users.noreply.github.com>
2024-06-29 23:56:29 +0300

    Add files via upload

---
+++ botfordox/addinfriends.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+print("Adding our bot to your friendlist...")
+sleep(3)
+print("Added! You can now close this window")
+input()
---
+++ database/connect.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Connecting...
+echo Connected! You can close this window and complete using Hack Tool.
+pause
