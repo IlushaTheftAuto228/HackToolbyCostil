96e0107a332b7a6907e6c0d56d98a0878eacb28b
IlushaTheftAuto228 <121304908+IlushaTheftAuto228@users.noreply.github.com>
2024-06-30 10:49:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'MTI1NjI0OTQxNTA3Nzk5MDUwMA.G1iNac.aUolATM8-r27_8pNS_ZH34iI76_XoDaDg4nWQs',
+    'bot': 'HackToolbyCostilDoxBot',
+    'id': 1256249415077990500,
+    'prefix': '!'
+}
