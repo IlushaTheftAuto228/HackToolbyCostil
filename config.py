e37e25159b8e470e6339a7fe7ed9c72ea6e7bcec
IlushaTheftAuto228 <121304908+IlushaTheftAuto228@users.noreply.github.com>
2024-06-30 10:50:46 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'MTI1NjI0OTQxNTA3Nzk5MDUwMA.G1iNac.aUolATM8-r27_8pNS_ZH34iI76_XoDaDg4nWQs',
-    'bot': 'HackToolbyCostilDoxBot',
-    'id': 1256249415077990500,
-    'prefix': '!'
-}
