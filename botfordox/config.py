17dbd5b4dfc274ec753db91ba21ea30f1537c424
IlushaTheftAuto228 <121304908+IlushaTheftAuto228@users.noreply.github.com>
2024-07-23 17:50:00 +0300

    Delete botfordox/config.py

--- botfordox/config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'MTI1NjI0OTQxNTA3Nzk5MDUwMA.GJSGY_.O9wIvGtdT_zg3Hfebcd_gbiEAy5gJkEIU3NIfw',
-    'bot': 'HackToolbyCostilDoxBot',
-    'id': 1256249415077990500,
-    'prefix': '!'
-}
