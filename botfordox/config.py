3e3f955044ec1b0faf5b6c8fcfb8f24fd886993b
IlushaTheftAuto228 <121304908+IlushaTheftAuto228@users.noreply.github.com>
2024-07-23 17:49:26 +0300

    Add files via upload

---
+++ botfordox/config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'MTI1NjI0OTQxNTA3Nzk5MDUwMA.GJSGY_.O9wIvGtdT_zg3Hfebcd_gbiEAy5gJkEIU3NIfw',
+    'bot': 'HackToolbyCostilDoxBot',
+    'id': 1256249415077990500,
+    'prefix': '!'
+}
